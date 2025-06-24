5c62b99504789171ffb25c28580a24d1dddd8a82
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-06-24 21:37:20 +0100

    hover

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ const cdiv = document.querySelector('#container');
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
+  //div.style.backgroundColor = "red";
+  div.className = 'grid-item';
+  div.addEventListener('mouseenter', () => {
+      div.classList.add('hovered');
+  });
   cdiv.appendChild(div);
 }
